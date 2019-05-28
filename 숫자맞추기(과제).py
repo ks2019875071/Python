cce1eefcd8ddff939caf05319466f4bccc2117d5
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2019-05-29 02:29:28 +0900

    Add files via upload

--- 숫자맞추기(과제).py
+++ 숫자맞추기(과제).py
@@ -2,6 +2,7 @@ import random
 
 ball = 0 ## 볼 횟수
 strike = 0 ## 스트라이크 횟수
+tried = 0 ##시도 횟수
 
 z_list = [] ## 랜덤으로 나오는 숫자 리스트
 for i in range (3) :
@@ -15,9 +16,17 @@ while strike!=3 :
     your_list.append(int(input("세번째 정수를 입력 하세요.")))
 
     for i in range (0, 3, 1) :
-        if z_list[i]==your_list[i] :
-            strike = strike+1
-    print("%d 스트라이크 입니다." %(strike))
+        for j in range(0, 3, 1) :
+            if z_list[i]==your_list[j] :
+                ball = ball+1
+    print("%d 볼 입니다." %(ball))
 
-if strike==3 :
+    for i in range(0, 3, 1) :
+            if z_list[i]==your_list[i] :
+                strike = strike+1
+    print("%d 스트라이크 입니다." %(strike))
+    tried = tried+1
+    
+if strike>=3 :
     print ("게임 종료")
+    print ("%d번 만에 성공했습니다!" %(tried))
